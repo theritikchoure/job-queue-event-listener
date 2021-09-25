853f1d7efd659ddfbfaeb65bf373b40bffb54f4d
Ritik <ritvikchoure65@gmail.com>
2021-09-25 10:31:30 +0530

    readmd

--- routes/web.php
+++ routes/web.php
@@ -41,10 +41,3 @@ Route::get('/event', function () {
     return $user->name;
 });
 
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
